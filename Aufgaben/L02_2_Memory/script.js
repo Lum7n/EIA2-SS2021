e1e214b7a50297733ec2ae5e3033b82d85b768fa
Ulla Herrmann <56406099+Lum7n@users.noreply.github.com>
2021-04-25 04:20:27 +0200

    Add files via upload

--- Aufgaben/L02_2_Memory/script.js
+++ Aufgaben/L02_2_Memory/script.js
@@ -8,10 +8,9 @@ var valentina;
             "K", "K", "L", "L", "M", "M", "N", "N", "O", "O",
             "P", "P", "Q", "Q", "R", "R", "S", "S", "T", "T",
             "U", "U", "V", "V", "W", "W", "X", "X"];
-        // let playcards: string[];
-        // let matches: string[];
-        // let chosenCards: string[];
-        //.addEventListener("click", showLetters);
+        let playcards = [];
+        let chosenCards = [];
+        let matches = [];
         let introduction;
         let container;
         let startButton;
@@ -23,42 +22,81 @@ var valentina;
             startButton.addEventListener("click", generateContent);
         }
         function generateContent() {
+            introduction.style.display = "none";
+            startButton.style.display = "none";
             let input = document.getElementById("number");
             let nPairs = parseInt(input.value);
-            console.log(nPairs);
+            // console.log(nPairs);
+            let amountCards = nPairs * 2;
+            for (let i = 0; i < amountCards; i++) {
+                let oneLetterArray = allcards.slice(i, i + 1);
+                let oneLetter = oneLetterArray.toString();
+                playcards.push(oneLetter);
+                // console.log(playcards);
+            }
+            shuffle(playcards);
+            console.log(playcards);
+            for (let k = 0; k < playcards.length; k++) {
+                let div = document.createElement("div");
+                div.classList.add("cardContainer");
+                div.style.backgroundColor = "#46469b";
+                let span = document.createElement("span");
+                span.innerHTML = playcards[k];
+                span.id = playcards[k];
+                span.style.color = "#46469b";
+                span.addEventListener("click", showLetters);
+                div.appendChild(span);
+                container.appendChild(div);
+            }
         }
-        // let allCards: string[] = ["A", "B", "C", "D", "E", "F"];
-        // console.log(allCards);
-        // let amountPairs: number = 4;
-        // let playingCardsOne: string[] = allCards.slice(0, amountPairs);
-        // let playingCardsTwo: string[] = playingCardsOne.slice(0, playingCardsOne.length);
-        // console.log(playingCardsOne);
-        // console.log(playingCardsTwo);
-        // getL(playingCardsOne);
-        // function getL(_playingCardsOne: string[]): void {
-        //     // for (let index = 0; index < array.length; index++) {
-        //     //     const element = array[index];
-        //     // }
-        //     let x: number = Math.random();
-        //     x = x * _playingCardsOne.length;
-        //     x = Math.floor(x);
-        //     let oneCard: string[] = _playingCardsOne.splice(x, 1);
-        //     console.log(oneCard);
-        //     console.log(playingCardsOne);
-        // }
-        // let pairsInput: string;
-        // window.addEventListener("load", handleLoad);
-        // function handleLoad(): void {
-        //     pairsInput = <string>prompt("how many pairs:", "5");
-        //     if (pairsInput) {
-        //         getCards();
-        //         startTimer();
-        //     }
-        // }
-        // function getCards(): void {
-        //     amountPairs = parseInt(pairsInput);
-        //     console.log(amountPairs);
-        // }
+        function showLetters(_event) {
+            let targetSpan = _event.target;
+            let targetID1 = this.id; //Help!!!! wie geht das ohne das der Linter mich gleich erhängt
+            let targetID2 = _event.target.id; // die beiden Methoden funktionieren, aber das kann ja nicht die richtige Lösung sein...? oder ?
+            console.log(targetSpan, targetID1, targetID2);
+            _event.target.style.color = "#ffffff";
+            chosenCards.push(targetID1);
+            if (chosenCards.length == 2) {
+                setTimeout(checkForMatch, 500);
+            }
+        }
+        function checkForMatch() {
+            if (chosenCards[0] == chosenCards[1]) {
+                alert("it's a match!");
+                //how can I find the two spans_ here less important cause they're right but....
+                let cardOneArray = chosenCards.splice(0, 1);
+                let cardOne = cardOneArray.toString();
+                matches.push(cardOne);
+                let cardTwoArray = chosenCards.splice(1, 1);
+                let cardTwo = cardTwoArray.toString();
+                matches.push(cardTwo);
+            }
+            else {
+                alert("wrong! try again.");
+                // here really important, i need to hide them again...
+            }
+            chosenCards = [];
+            if (matches.length == playcards.length) {
+                alert("you won!!!");
+            }
+        }
+        // basierend auf Fisher-Yates Shuffle
+        function shuffle(_array) {
+            let currentIndex = _array.length;
+            let temporaryValue;
+            let randomIndex;
+            // Solange noch Elemente vorhanden sind ...
+            while (0 !== currentIndex) {
+                // ein vorhandenes Element auswählen und ...
+                randomIndex = Math.floor(Math.random() * currentIndex);
+                currentIndex -= 1;
+                // mit dem Aktuellen tauschen.
+                temporaryValue = _array[currentIndex];
+                _array[currentIndex] = _array[randomIndex];
+                _array[randomIndex] = temporaryValue;
+            }
+            return _array;
+        }
     })(Memory = valentina.Memory || (valentina.Memory = {}));
 })(valentina || (valentina = {}));
 //# sourceMappingURL=script.js.map
--- Aufgaben/L02_2_Memory/script.ts
+++ Aufgaben/L02_2_Memory/script.ts
@@ -1,16 +1,14 @@
 namespace valentina.Memory {
 
     let allcards: string[] = ["A", "A", "B", "B", "C", "C", "D", "D", "E", "E",
-                                "F", "F", "G", "G", "H", "H", "I", "I", "J", "J",
-                                "K", "K", "L", "L", "M", "M", "N", "N", "O", "O",
-                                "P", "P", "Q", "Q", "R", "R", "S", "S", "T", "T",
-                                "U", "U", "V", "V", "W", "W", "X", "X"];
+        "F", "F", "G", "G", "H", "H", "I", "I", "J", "J",
+        "K", "K", "L", "L", "M", "M", "N", "N", "O", "O",
+        "P", "P", "Q", "Q", "R", "R", "S", "S", "T", "T",
+        "U", "U", "V", "V", "W", "W", "X", "X"];
 
-    // let playcards: string[];
-    // let matches: string[];
-    // let chosenCards: string[];
-
-    //.addEventListener("click", showLetters);
+    let playcards: string[] = [];
+    let chosenCards: string[] = [];
+    let matches: string[] = [];
 
     let introduction: HTMLDivElement;
     let container: HTMLDivElement;
@@ -29,67 +27,103 @@ namespace valentina.Memory {
 
 
     function generateContent(): void {
+
+        introduction.style.display = "none";
+        startButton.style.display = "none";
+
         let input: HTMLInputElement = <HTMLInputElement>document.getElementById("number");
         let nPairs: number = <number>parseInt(input.value);
-        console.log(nPairs);
+        // console.log(nPairs);
+        let amountCards: number = nPairs * 2;
+
+        for (let i: number = 0; i < amountCards; i++) {
+            let oneLetterArray: string[] = allcards.slice(i, i + 1);
+            let oneLetter: string = oneLetterArray.toString();
+            playcards.push(oneLetter);
+            // console.log(playcards);
+        }
+        shuffle(playcards);
+        console.log(playcards);
+
+        for (let k: number = 0; k < playcards.length; k++) {
+            let div: HTMLDivElement = document.createElement("div");
+            div.classList.add("cardContainer");
+            div.style.backgroundColor = "#46469b";
+
+            let span: HTMLSpanElement = document.createElement("span");
+            span.innerHTML = playcards[k];
+            span.id = playcards[k];
+            span.style.color = "#46469b";
+            span.addEventListener("click", showLetters);
+
+            div.appendChild(span);
+            container.appendChild(div);
+        }
     }
 
+    function showLetters(_event: Event): void {
 
+        let targetSpan: EventTarget = <EventTarget>_event.target;
+        let targetID1: string = this.id;                                //Help!!!! wie geht das ohne das der Linter mich gleich erhängt
+        let targetID2: string = _event.target.id;                       // die beiden Methoden funktionieren, aber das kann ja nicht die richtige Lösung sein...? oder ?
 
+        console.log(targetSpan, targetID1, targetID2);
 
+        _event.target.style.color = "#ffffff";
+        chosenCards.push(targetID1);
 
+        if (chosenCards.length == 2) {
+            setTimeout(checkForMatch, 500);
+        }
+    }
 
+    function checkForMatch(): void {
 
-    // let allCards: string[] = ["A", "B", "C", "D", "E", "F"];
+        if (chosenCards[0] == chosenCards[1]) {
+            alert("it's a match!");
 
-    // console.log(allCards);
+        //how can I find the two spans_ here less important cause they're right but....
 
-    // let amountPairs: number = 4;
-    // let playingCardsOne: string[] = allCards.slice(0, amountPairs);
-    // let playingCardsTwo: string[] = playingCardsOne.slice(0, playingCardsOne.length);
+            let cardOneArray: string[] = chosenCards.splice(0, 1);
+            let cardOne: string = cardOneArray.toString();
+            matches.push(cardOne);
+            let cardTwoArray: string[] = chosenCards.splice(1, 1);
+            let cardTwo: string = cardTwoArray.toString();
+            matches.push(cardTwo);
 
-    // console.log(playingCardsOne);
-    // console.log(playingCardsTwo);
+        } else {
+            alert("wrong! try again.");
 
-    // getL(playingCardsOne);
+        // here really important, i need to hide them again...
+        }
 
-    // function getL(_playingCardsOne: string[]): void {
+        chosenCards = [];
 
-    //     // for (let index = 0; index < array.length; index++) {
-    //     //     const element = array[index];
+        if (matches.length == playcards.length) {
 
-    //     // }
+            alert("you won!!!");
+        }
+    }
 
-    //     let x: number = Math.random();
-    //     x = x * _playingCardsOne.length;
-    //     x = Math.floor(x);
+    // basierend auf Fisher-Yates Shuffle
+    function shuffle(_array: string[]): string[] {
 
-    //     let oneCard: string[] = _playingCardsOne.splice(x, 1);
+        let currentIndex: number = _array.length;
+        let temporaryValue: string;
+        let randomIndex: number;
 
-    //     console.log(oneCard);
-    //     console.log(playingCardsOne);
+        // Solange noch Elemente vorhanden sind ...
+        while (0 !== currentIndex) {
 
-    // }
+            // ein vorhandenes Element auswählen und ...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
 
-    // let pairsInput: string;
-
-    // window.addEventListener("load", handleLoad);
-
-    // function handleLoad(): void {
-
-    //     pairsInput = <string>prompt("how many pairs:", "5");
-
-    //     if (pairsInput) {
-    //         getCards();
-    //         startTimer();
-    //     }
-    // }
-
-    // function getCards(): void {
-
-    //     amountPairs = parseInt(pairsInput);
-    //     console.log(amountPairs);
-    // }
-
-
+            // mit dem Aktuellen tauschen.
+            temporaryValue = _array[currentIndex];
+            _array[currentIndex] = _array[randomIndex];
+            _array[randomIndex] = temporaryValue;
+        }
+        return _array;
+    }
 }
