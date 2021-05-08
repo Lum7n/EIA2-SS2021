55492bf8c80b7db70442d2a4938dbd13e56dd591
Ulla Herrmann <56406099+Lum7n@users.noreply.github.com>
2021-05-08 23:54:05 +0200

    Add files via upload

---
+++ Aufgaben/script.ts
@@ -0,0 +1,14 @@
+namespace GenerativeKunst {
+
+    window.addEventListener("load", init);
+
+    function init(_event: Event): void {
+
+        let canvas: HTMLCanvasElement = <HTMLCanvasElement>document.querySelector("canvas");
+        let crc2: CanvasRenderingContext2D = <CanvasRenderingContext2D>canvas.getContext("2d");
+
+
+
+    }
+
+}
