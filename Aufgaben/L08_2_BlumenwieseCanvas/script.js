0fb1c58c480a84ba58b870699ce66aad7ca2342f
Ulla Herrmann <56406099+Lum7n@users.noreply.github.com>
2021-05-16 01:22:22 +0200

    Add files via upload

--- Aufgaben/L08_2_BlumenwieseCanvas/script.js
+++ Aufgaben/L08_2_BlumenwieseCanvas/script.js
@@ -15,13 +15,12 @@ var Blumenwiese_L08;
         canvasHeight = crc2.canvas.height;
         // border
         crc2.strokeRect(0, 0, canvasWidth, canvasHeight);
-        // let horizon: number = crc2.canvas.height * golden;
-        // let posMountains: Vector = { x: 0, y: horizon };
-        // drawBackground();
-        // drawSun({ x: 100, y: 75 });
+        let posMountains = { x: 0, y: canvasHeight / 2 };
+        drawBackground();
+        drawSun({ x: golden, y: 1 - golden });
         // drawCloud({ x: 500, y: 125 }, { x: 250, y: 75 });
-        // drawMountains(posMountains, 75, 200, "grey", "white");
-        // drawMountains(posMountains, 50, 150, "grey", "lightgrey");
+        drawMountains(posMountains, 0.55, 0.75, "grey", "white");
+        drawMountains(posMountains, 0.5, 0.7, "grey", "lightgrey");
     }
     function canvasSize() {
         let windowWidth = window.innerWidth;
@@ -30,29 +29,34 @@ var Blumenwiese_L08;
         canvas.height = windowHeight - 100;
         crc2.strokeRect(0, 0, canvasWidth, canvasHeight);
     }
-    // function drawBackground(): void {
-    //     console.log("Background");
-    //     let gradient: CanvasGradient = crc2.createLinearGradient(0, 0, 0, crc2.canvas.height);
-    //     gradient.addColorStop(0, "lightblue");
-    //     gradient.addColorStop(golden, "white");
-    //     gradient.addColorStop(1, "HSL(100, 80%, 30%)");
-    //     crc2.fillStyle = gradient;
-    //     crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
-    // }
-    // function drawSun(_position: Vector): void {
-    //     console.log("Sun", _position);
-    //     let r1: number = 30;
-    //     let r2: number = 150;
-    //     let gradient: CanvasGradient = crc2.createRadialGradient(0, 0, r1, 0, 0, r2);
-    //     gradient.addColorStop(0, "HSLA(60, 100%, 90%, 1)");
-    //     gradient.addColorStop(1, "HSLA(60, 100%, 50%, 0)");
-    //     crc2.save();
-    //     crc2.translate(_position.x, _position.y);
-    //     crc2.fillStyle = gradient;
-    //     crc2.arc(0, 0, r2, 0, 2 * Math.PI);
-    //     crc2.fill();
-    //     crc2.restore();
-    // }
+    function drawBackground() {
+        console.log("Background");
+        let gradient = crc2.createLinearGradient(0, 0, 0, crc2.canvas.height);
+        gradient.addColorStop(0, "#8fb7db");
+        gradient.addColorStop(0.25, "#f5ddda");
+        gradient.addColorStop(1 - golden, "#fcd087");
+        gradient.addColorStop(1, "#388a0f");
+        crc2.fillStyle = gradient;
+        crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+    }
+    function drawSun(_position) {
+        console.log("Sun", _position);
+        let r1 = 30;
+        let r2 = 150;
+        let gradient = crc2.createRadialGradient(0, 0, r1, 0, 0, r2);
+        let x = _position.x * canvasWidth;
+        let y = _position.y * canvasHeight;
+        gradient.addColorStop(0, "#fffcdd");
+        gradient.addColorStop(0.3, "#ffd72b66");
+        gradient.addColorStop(0.7, "#fe9d0026");
+        gradient.addColorStop(1, "#fe9d0000");
+        crc2.save();
+        crc2.translate(x, y);
+        crc2.fillStyle = gradient;
+        crc2.arc(0, 0, r2, 0, 2 * Math.PI);
+        crc2.fill();
+        crc2.restore();
+    }
     // function drawCloud(_position: Vector, _size: Vector): void {
     //     console.log("Cloud", _position, _size);
     //     let nParticles: number = 20;
@@ -75,29 +79,31 @@ var Blumenwiese_L08;
     //     }
     //     crc2.restore();
     // }
-    // function drawMountains(_position: Vector, _min: number, _max: number, _colorLow: string, _colorHigh: string): void {
-    //     console.log("Mountains", _position, _min, _max);
-    //     let stepMin: number = 50;
-    //     let stepMax: number = 150;
-    //     let x: number = 0;
-    //     crc2.save();
-    //     crc2.translate(_position.x, _position.y);
-    //     crc2.beginPath();
-    //     crc2.moveTo(0, 0);
-    //     crc2.lineTo(0, -_max);
-    //     do {
-    //         x += stepMin + Math.random() * (stepMax - stepMin);
-    //         let y: number = -_min - Math.random() * (_max - _min);
-    //         crc2.lineTo(x, y);
-    //     } while (x < crc2.canvas.width);
-    //     crc2.lineTo(x, 0);
-    //     crc2.closePath();
-    //     let gradient: CanvasGradient = crc2.createLinearGradient(0, 0, 0, -_max);
-    //     gradient.addColorStop(0, _colorLow);
-    //     gradient.addColorStop(0.7, _colorHigh);
-    //     crc2.fillStyle = gradient;
-    //     crc2.fill();
-    //     crc2.restore();
-    // }
+    function drawMountains(_position, _min, _max, _colorLow, _colorHigh) {
+        console.log("Mountains", _position, _min, _max);
+        let stepMin = 50;
+        let stepMax = 150;
+        let x = 0;
+        let minHeight = (_min - 0.5) * canvasHeight;
+        let maxHeight = (_max - 0.5) * canvasHeight;
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
+        crc2.beginPath();
+        crc2.moveTo(0, 0);
+        crc2.lineTo(0, -maxHeight);
+        do {
+            x += stepMin + Math.random() * (stepMax - stepMin);
+            let y = -minHeight - Math.random() * (maxHeight - minHeight);
+            crc2.lineTo(x, y);
+        } while (x < crc2.canvas.width);
+        crc2.lineTo(x, 0);
+        crc2.closePath();
+        let gradient = crc2.createLinearGradient(0, 0, 0, -maxHeight);
+        gradient.addColorStop(0, _colorLow);
+        gradient.addColorStop(0.7, _colorHigh);
+        crc2.fillStyle = gradient;
+        crc2.fill();
+        crc2.restore();
+    }
 })(Blumenwiese_L08 || (Blumenwiese_L08 = {}));
 //# sourceMappingURL=script.js.map
--- Aufgaben/L08_2_BlumenwieseCanvas/script.ts
+++ Aufgaben/L08_2_BlumenwieseCanvas/script.ts
@@ -27,15 +27,15 @@ namespace Blumenwiese_L08 {
         // border
         crc2.strokeRect(0, 0, canvasWidth, canvasHeight);
 
-        // let horizon: number = crc2.canvas.height * golden;
-        // let posMountains: Vector = { x: 0, y: horizon };
+        let posMountains: Vector = { x: 0, y: canvasHeight / 2};
 
 
-        // drawBackground();
-        // drawSun({ x: 100, y: 75 });
+
+        drawBackground();
+        drawSun({ x: golden, y: 1 - golden });
         // drawCloud({ x: 500, y: 125 }, { x: 250, y: 75 });
-        // drawMountains(posMountains, 75, 200, "grey", "white");
-        // drawMountains(posMountains, 50, 150, "grey", "lightgrey");
+        drawMountains(posMountains, 0.55, 0.75, "grey", "white");
+        drawMountains(posMountains, 0.5, 0.7, "grey", "lightgrey");
 
     }
 
@@ -50,35 +50,41 @@ namespace Blumenwiese_L08 {
         crc2.strokeRect(0, 0, canvasWidth, canvasHeight);
     }
 
-    // function drawBackground(): void {
-    //     console.log("Background");
+    function drawBackground(): void {
+        console.log("Background");
 
-    //     let gradient: CanvasGradient = crc2.createLinearGradient(0, 0, 0, crc2.canvas.height);
-    //     gradient.addColorStop(0, "lightblue");
-    //     gradient.addColorStop(golden, "white");
-    //     gradient.addColorStop(1, "HSL(100, 80%, 30%)");
+        let gradient: CanvasGradient = crc2.createLinearGradient(0, 0, 0, crc2.canvas.height);
+        gradient.addColorStop(0, "#8fb7db");
+        gradient.addColorStop(0.25, "#f5ddda");
+        gradient.addColorStop(1 - golden, "#fcd087");
+        gradient.addColorStop(1, "#388a0f");
 
-    //     crc2.fillStyle = gradient;
-    //     crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
-    // }
+        crc2.fillStyle = gradient;
+        crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+    }
 
-    // function drawSun(_position: Vector): void {
-    //     console.log("Sun", _position);
+    function drawSun(_position: Vector): void {
+        console.log("Sun", _position);
 
-    //     let r1: number = 30;
-    //     let r2: number = 150;
-    //     let gradient: CanvasGradient = crc2.createRadialGradient(0, 0, r1, 0, 0, r2);
+        let r1: number = 30;
+        let r2: number = 150;
+        let gradient: CanvasGradient = crc2.createRadialGradient(0, 0, r1, 0, 0, r2);
 
-    //     gradient.addColorStop(0, "HSLA(60, 100%, 90%, 1)");
-    //     gradient.addColorStop(1, "HSLA(60, 100%, 50%, 0)");
+        let x: number = _position.x * canvasWidth;
+        let y: number = _position.y * canvasHeight;
+  
+        gradient.addColorStop(0, "#fffcdd");
+        gradient.addColorStop(0.3, "#ffd72b66");
+        gradient.addColorStop(0.7, "#fe9d0026");
+        gradient.addColorStop(1, "#fe9d0000");
 
-    //     crc2.save();
-    //     crc2.translate(_position.x, _position.y);
-    //     crc2.fillStyle = gradient;
-    //     crc2.arc(0, 0, r2, 0, 2 * Math.PI);
-    //     crc2.fill();
-    //     crc2.restore();
-    // }
+        crc2.save();
+        crc2.translate(x, y);
+        crc2.fillStyle = gradient;
+        crc2.arc(0, 0, r2, 0, 2 * Math.PI);
+        crc2.fill();
+        crc2.restore();
+    }
 
 
     // function drawCloud(_position: Vector, _size: Vector): void {
@@ -108,37 +114,39 @@ namespace Blumenwiese_L08 {
     //     crc2.restore();
     // }
 
-    // function drawMountains(_position: Vector, _min: number, _max: number, _colorLow: string, _colorHigh: string): void {
-    //     console.log("Mountains", _position, _min, _max);
-    //     let stepMin: number = 50;
-    //     let stepMax: number = 150;
-    //     let x: number = 0;
+    function drawMountains(_position: Vector, _min: number, _max: number, _colorLow: string, _colorHigh: string): void {
+        console.log("Mountains", _position, _min, _max);
+        let stepMin: number = 50;
+        let stepMax: number = 150;
+        let x: number = 0;
+        let minHeight: number = (_min - 0.5) * canvasHeight;
+        let maxHeight: number = (_max - 0.5) * canvasHeight;
 
-    //     crc2.save();
-    //     crc2.translate(_position.x, _position.y);
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
 
-    //     crc2.beginPath();
-    //     crc2.moveTo(0, 0);
-    //     crc2.lineTo(0, -_max);
+        crc2.beginPath();
+        crc2.moveTo(0, 0);
+        crc2.lineTo(0, -maxHeight);
 
-    //     do {
-    //         x += stepMin + Math.random() * (stepMax - stepMin);
-    //         let y: number = -_min - Math.random() * (_max - _min);
+        do {
+            x += stepMin + Math.random() * (stepMax - stepMin);
+            let y: number = -minHeight - Math.random() * (maxHeight - minHeight);
 
-    //         crc2.lineTo(x, y);
-    //     } while (x < crc2.canvas.width);
+            crc2.lineTo(x, y);
+        } while (x < crc2.canvas.width);
 
-    //     crc2.lineTo(x, 0);
-    //     crc2.closePath();
+        crc2.lineTo(x, 0);
+        crc2.closePath();
 
-    //     let gradient: CanvasGradient = crc2.createLinearGradient(0, 0, 0, -_max);
-    //     gradient.addColorStop(0, _colorLow);
-    //     gradient.addColorStop(0.7, _colorHigh);
+        let gradient: CanvasGradient = crc2.createLinearGradient(0, 0, 0, -maxHeight);
+        gradient.addColorStop(0, _colorLow);
+        gradient.addColorStop(0.7, _colorHigh);
 
-    //     crc2.fillStyle = gradient;
-    //     crc2.fill();
+        crc2.fillStyle = gradient;
+        crc2.fill();
 
-    //     crc2.restore();
-    // }
+        crc2.restore();
+    }
 
 }
