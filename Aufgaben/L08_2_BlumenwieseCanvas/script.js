c7359c9f6ba86e02376d2422059cb6a0d7433478
Ulla Herrmann <56406099+Lum7n@users.noreply.github.com>
2021-05-16 03:17:12 +0200

    Add files via upload

--- Aufgaben/L08_2_BlumenwieseCanvas/script.js
+++ Aufgaben/L08_2_BlumenwieseCanvas/script.js
@@ -18,9 +18,9 @@ var Blumenwiese_L08;
         let posMountains = { x: 0, y: canvasHeight / 2 };
         drawBackground();
         drawSun({ x: golden, y: 1 - golden });
-        // drawCloud({ x: 500, y: 125 }, { x: 250, y: 75 });
-        drawMountains(posMountains, 0.55, 0.75, "grey", "white");
-        drawMountains(posMountains, 0.5, 0.7, "grey", "lightgrey");
+        drawClouds();
+        drawMountains(posMountains, 0.55, 0.70, "#7f91b3", "#4f4f69", "#9e9eb8");
+        drawMountains(posMountains, 0.5, 0.65, "#67688a", "#3b3e4d", "#9a8fa6");
     }
     function canvasSize() {
         let windowWidth = window.innerWidth;
@@ -57,32 +57,57 @@ var Blumenwiese_L08;
         crc2.fill();
         crc2.restore();
     }
-    // function drawCloud(_position: Vector, _size: Vector): void {
-    //     console.log("Cloud", _position, _size);
-    //     let nParticles: number = 20;
-    //     let radiusParticle: number = 50;
-    //     let particle: Path2D = new Path2D();
-    //     let gradient: CanvasGradient = crc2.createRadialGradient(0, 0, 0, 0, 0, radiusParticle);
-    //     particle.arc(0, 0, radiusParticle, 0, 2 * Math.PI);
-    //     gradient.addColorStop(0, "HSLA(0, 100%, 100%, 0.5)");
-    //     gradient.addColorStop(1, "HSLA(0, 100%, 100%, 0)");
-    //     crc2.save();
-    //     crc2.translate(_position.x, _position.y);
-    //     crc2.fillStyle = gradient;
-    //     for (let drawn: number = 0; drawn < nParticles; drawn++) {
-    //         crc2.save();
-    //         let x: number = (Math.random() - 0.5) * _size.x;
-    //         let y: number = - (Math.random() * _size.y);
-    //         crc2.translate(x, y);
-    //         crc2.fill(particle);
-    //         crc2.restore();
-    //     }
-    //     crc2.restore();
-    // }
-    function drawMountains(_position, _min, _max, _colorLow, _colorHigh) {
+    function drawClouds() {
+        let sizeMin = { x: 0.17, y: 0.02 };
+        let sizeMax = { x: 0.27, y: 0.03 };
+        let nParticlesMin = 75;
+        let nParticlesMax = 120;
+        let cloudAmount = Math.floor(canvasWidth / 100);
+        let horizon = (1 - golden) * canvasHeight;
+        let skyPart = horizon / 5;
+        // drawCloud({ x: 200, y: 125 }, sizeMax, nParticlesMax);
+        // drawCloud({ x: 200, y: 125 }, sizeMin, nParticlesMin);
+        while (cloudAmount != 0) {
+            console.log(cloudAmount);
+            let positionX = 100 * (Math.random() * cloudAmount + 1);
+            let positionY = skyPart + (Math.random() * (skyPart * 3));
+            if (cloudAmount >= 4) {
+                drawCloud({ x: positionX, y: positionY }, sizeMax, nParticlesMax);
+            }
+            else {
+                drawCloud({ x: positionX, y: positionY }, sizeMin, nParticlesMin);
+            }
+            cloudAmount--;
+        }
+    }
+    function drawCloud(_position, _size, _nParticles) {
+        console.log("Cloud", _position, _size);
+        let sizeX = _size.x * canvasWidth;
+        let sizeY = _size.y * canvasHeight;
+        let radius1Particle = 5;
+        let radius2Particle = 15;
+        let particle = new Path2D();
+        let gradient = crc2.createRadialGradient(0, 0, 0, 0, 0, radius2Particle);
+        particle.ellipse(0, 0, radius1Particle, radius2Particle, Math.PI / 2, 0, 2 * Math.PI);
+        gradient.addColorStop(0, "#ffded980");
+        gradient.addColorStop(1, "#ffffff00");
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
+        crc2.fillStyle = gradient;
+        for (let drawn = 0; drawn < _nParticles; drawn++) {
+            crc2.save();
+            let x = (Math.random() - 0.5) * sizeX;
+            let y = -(Math.random() * sizeY);
+            crc2.translate(x, y);
+            crc2.fill(particle);
+            crc2.restore();
+        }
+        crc2.restore();
+    }
+    function drawMountains(_position, _min, _max, _colorLow, _colorHigh, _colorTop) {
         console.log("Mountains", _position, _min, _max);
         let stepMin = 50;
-        let stepMax = 150;
+        let stepMax = 100;
         let x = 0;
         let minHeight = (_min - 0.5) * canvasHeight;
         let maxHeight = (_max - 0.5) * canvasHeight;
@@ -90,7 +115,7 @@ var Blumenwiese_L08;
         crc2.translate(_position.x, _position.y);
         crc2.beginPath();
         crc2.moveTo(0, 0);
-        crc2.lineTo(0, -maxHeight);
+        crc2.lineTo(0, -maxHeight * 0.7);
         do {
             x += stepMin + Math.random() * (stepMax - stepMin);
             let y = -minHeight - Math.random() * (maxHeight - minHeight);
@@ -101,6 +126,8 @@ var Blumenwiese_L08;
         let gradient = crc2.createLinearGradient(0, 0, 0, -maxHeight);
         gradient.addColorStop(0, _colorLow);
         gradient.addColorStop(0.7, _colorHigh);
+        gradient.addColorStop(0.8, _colorHigh);
+        gradient.addColorStop(1, _colorTop);
         crc2.fillStyle = gradient;
         crc2.fill();
         crc2.restore();
--- Aufgaben/L08_2_BlumenwieseCanvas/script.ts
+++ Aufgaben/L08_2_BlumenwieseCanvas/script.ts
@@ -27,15 +27,13 @@ namespace Blumenwiese_L08 {
         // border
         crc2.strokeRect(0, 0, canvasWidth, canvasHeight);
 
-        let posMountains: Vector = { x: 0, y: canvasHeight / 2};
-
-
+        let posMountains: Vector = { x: 0, y: canvasHeight / 2 };
 
         drawBackground();
         drawSun({ x: golden, y: 1 - golden });
-        // drawCloud({ x: 500, y: 125 }, { x: 250, y: 75 });
-        drawMountains(posMountains, 0.55, 0.75, "grey", "white");
-        drawMountains(posMountains, 0.5, 0.7, "grey", "lightgrey");
+        drawClouds();
+        drawMountains(posMountains, 0.55, 0.70, "#7f91b3", "#4f4f69", "#9e9eb8");
+        drawMountains(posMountains, 0.5, 0.65, "#67688a", "#3b3e4d", "#9a8fa6");
 
     }
 
@@ -72,7 +70,7 @@ namespace Blumenwiese_L08 {
 
         let x: number = _position.x * canvasWidth;
         let y: number = _position.y * canvasHeight;
-  
+
         gradient.addColorStop(0, "#fffcdd");
         gradient.addColorStop(0.3, "#ffd72b66");
         gradient.addColorStop(0.7, "#fe9d0026");
@@ -86,38 +84,70 @@ namespace Blumenwiese_L08 {
         crc2.restore();
     }
 
+    function drawClouds(): void {
 
-    // function drawCloud(_position: Vector, _size: Vector): void {
-    //     console.log("Cloud", _position, _size);
+        let sizeMin: Vector = { x: 0.17, y: 0.02 };
+        let sizeMax: Vector = { x: 0.27, y: 0.03 };
+        let nParticlesMin: number = 75;
+        let nParticlesMax: number = 120;
 
-    //     let nParticles: number = 20;
-    //     let radiusParticle: number = 50;
-    //     let particle: Path2D = new Path2D();
-    //     let gradient: CanvasGradient = crc2.createRadialGradient(0, 0, 0, 0, 0, radiusParticle);
+        let cloudAmount: number = Math.floor(canvasWidth / 100);
+        let horizon: number = (1 - golden) * canvasHeight;
+        let skyPart: number = horizon / 5;
 
-    //     particle.arc(0, 0, radiusParticle, 0, 2 * Math.PI);
-    //     gradient.addColorStop(0, "HSLA(0, 100%, 100%, 0.5)");
-    //     gradient.addColorStop(1, "HSLA(0, 100%, 100%, 0)");
+        // drawCloud({ x: 200, y: 125 }, sizeMax, nParticlesMax);
+        // drawCloud({ x: 200, y: 125 }, sizeMin, nParticlesMin);
 
-    //     crc2.save();
-    //     crc2.translate(_position.x, _position.y);
-    //     crc2.fillStyle = gradient;
+        while (cloudAmount != 0) {
+            console.log(cloudAmount);
 
-    //     for (let drawn: number = 0; drawn < nParticles; drawn++) {
-    //         crc2.save();
-    //         let x: number = (Math.random() - 0.5) * _size.x;
-    //         let y: number = - (Math.random() * _size.y);
-    //         crc2.translate(x, y);
-    //         crc2.fill(particle);
-    //         crc2.restore();
-    //     }
-    //     crc2.restore();
-    // }
+            let positionX: number = 100 * (Math.random() * cloudAmount + 1);
+            let positionY: number = skyPart + (Math.random() * (skyPart * 3));
 
-    function drawMountains(_position: Vector, _min: number, _max: number, _colorLow: string, _colorHigh: string): void {
+            if (cloudAmount >= 4) {
+                drawCloud({ x: positionX, y: positionY }, sizeMax, nParticlesMax);
+            } else {
+                drawCloud({ x: positionX, y: positionY }, sizeMin, nParticlesMin);
+            }
+            cloudAmount--;
+
+        }
+    }
+
+    function drawCloud(_position: Vector, _size: Vector, _nParticles: number): void {
+        console.log("Cloud", _position, _size);
+
+        let sizeX: number = _size.x * canvasWidth;
+        let sizeY: number = _size.y * canvasHeight;
+
+        let radius1Particle: number = 5;
+        let radius2Particle: number = 15;
+        let particle: Path2D = new Path2D();
+        let gradient: CanvasGradient = crc2.createRadialGradient(0, 0, 0, 0, 0, radius2Particle);
+
+        particle.ellipse(0, 0, radius1Particle, radius2Particle, Math.PI / 2, 0, 2 * Math.PI);
+        gradient.addColorStop(0, "#ffded980");
+        gradient.addColorStop(1, "#ffffff00");
+
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
+        crc2.fillStyle = gradient;
+
+        for (let drawn: number = 0; drawn < _nParticles; drawn++) {
+            crc2.save();
+            let x: number = (Math.random() - 0.5) * sizeX;
+            let y: number = - (Math.random() * sizeY);
+            crc2.translate(x, y);
+            crc2.fill(particle);
+            crc2.restore();
+        }
+        crc2.restore();
+    }
+
+    function drawMountains(_position: Vector, _min: number, _max: number, _colorLow: string, _colorHigh: string, _colorTop: string): void {
         console.log("Mountains", _position, _min, _max);
         let stepMin: number = 50;
-        let stepMax: number = 150;
+        let stepMax: number = 100;
         let x: number = 0;
         let minHeight: number = (_min - 0.5) * canvasHeight;
         let maxHeight: number = (_max - 0.5) * canvasHeight;
@@ -127,7 +157,7 @@ namespace Blumenwiese_L08 {
 
         crc2.beginPath();
         crc2.moveTo(0, 0);
-        crc2.lineTo(0, -maxHeight);
+        crc2.lineTo(0, -maxHeight * 0.7);
 
         do {
             x += stepMin + Math.random() * (stepMax - stepMin);
@@ -142,6 +172,8 @@ namespace Blumenwiese_L08 {
         let gradient: CanvasGradient = crc2.createLinearGradient(0, 0, 0, -maxHeight);
         gradient.addColorStop(0, _colorLow);
         gradient.addColorStop(0.7, _colorHigh);
+        gradient.addColorStop(0.8, _colorHigh);
+        gradient.addColorStop(1, _colorTop);
 
         crc2.fillStyle = gradient;
         crc2.fill();
